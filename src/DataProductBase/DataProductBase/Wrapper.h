6ec9a5d955b453b2d393cca13fad78426a968af9
Chris Jones <chrisdjones15@gmail.com>
2026-03-11 11:25:56 -0500

    Have Wrapper hold a product

--- src/DataProductBase/DataProductBase/Wrapper.h
+++ src/DataProductBase/DataProductBase/Wrapper.h
@@ -2,9 +2,20 @@
 #define DataProductBase_Wrapper_h
 
 #include "DataProductBase/WrapperBase.h"
+#include <optional>
 namespace edm {
   template <typename T>
-  class Wrapper : public WrapperBase {};
+  class Wrapper : public WrapperBase {
+  public:
+    Wrapper() = default;
+    explicit Wrapper(T const& iData) : data_(iData) {}
+    explicit Wrapper(T&& iData) : data_(std::move(iData)) {}
+    ~Wrapper() override = default;
+
+    T const& product() const { return data_.value(); }
+  private:
+    std::optional<T> data_;
+  };
 }  // namespace edm
 
 #endif
