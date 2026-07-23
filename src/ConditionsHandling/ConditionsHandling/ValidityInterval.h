8af4a5481b73758c393625f030122c828d8d4c1c
Chris Jones <chrisdjones15@gmail.com>
2026-07-23 15:59:34 -0500

    Added ValidityInterval::overlapWith

--- src/ConditionsHandling/ConditionsHandling/ValidityInterval.h
+++ src/ConditionsHandling/ConditionsHandling/ValidityInterval.h
@@ -32,6 +32,34 @@ namespace edm {
     bool operator==(const ValidityInterval& other) const { return other.begin_ == begin_ && other.end_ == end_; }
     bool operator!=(const ValidityInterval& other) const { return !(*this == other); }
 
+    constexpr void overlapWith(ValidityInterval const& other) {
+      if (other.begin_.size() == 0) {
+        //other is an empty interval, nothing to do
+        return;
+      }
+      if (begin_.size() == 0) {
+        //If this is an empty interval, just copy the other
+        *this = other;
+        return;
+      }
+
+      begin_ = std::max(begin_, other.begin_);
+      //open intervals should remain open, so if either is open, the union should be open
+      if (end_.size() == 0) {
+        return;
+      }
+      if (other.end_.size() == 0) {
+        end_ = other.end_;
+        return;
+      }
+      end_ = std::min(end_, other.end_);
+      if (end_ < begin_) {
+        //if the end is less than the begin, then this is an empty interval
+        begin_ = TransitionRecordID{};
+        end_ = TransitionRecordID{};
+      }
+    }
+
   private:
     TransitionRecordID begin_;
     TransitionRecordID end_;
--- src/ConditionsHandling/test/test_catch2_validityinterval.cc
+++ src/ConditionsHandling/test/test_catch2_validityinterval.cc
@@ -128,5 +128,73 @@ namespace {
       REQUIRE(interval1 == interval2);
       REQUIRE(interval1 != interval3);
     }
+
+    SECTION("overlapWith with non-overlapping intervals") {
+      auto runID1 = edm::TransitionRecordID(1U);
+      auto runID2 = edm::TransitionRecordID(2U);
+      auto runID3 = edm::TransitionRecordID(3U);
+      auto runID4 = edm::TransitionRecordID(4U);
+      edm::ValidityInterval interval1(runID1, runID2);
+      edm::ValidityInterval interval2(runID3, runID4);
+      interval1.overlapWith(interval2);
+      REQUIRE(interval1.begin().size() == 0);
+      REQUIRE(interval1.end().size() == 0);
+    }
+
+    SECTION("overlapWith with overlapping intervals") {
+      auto runID1 = edm::TransitionRecordID(1U);
+      auto runID2 = edm::TransitionRecordID(2U);
+      auto runID3 = edm::TransitionRecordID(3U);
+      auto runID4 = edm::TransitionRecordID(4U);
+      edm::ValidityInterval interval1(runID1, runID4);
+      edm::ValidityInterval interval2(runID2, runID3);
+      interval1.overlapWith(interval2);
+      REQUIRE(interval1.begin() == runID2);
+      REQUIRE(interval1.end() == runID3);
+    }
+
+    SECTION("overlapWith with one empty interval") {
+      auto runID1 = edm::TransitionRecordID(1U);
+      auto runID2 = edm::TransitionRecordID(2U);
+      edm::ValidityInterval interval1;
+      edm::ValidityInterval interval2(runID1, runID2);
+      interval1.overlapWith(interval2);
+      REQUIRE(interval1.begin() == runID1);
+      REQUIRE(interval1.end() == runID2);
+    }
+
+    SECTION("overlapWith with open-ended intervals") {
+      auto runID1 = edm::TransitionRecordID(1U);
+      auto runID2 = edm::TransitionRecordID(2U);
+      auto runID3 = edm::TransitionRecordID(3U);
+      edm::ValidityInterval interval1(runID1, edm::TransitionRecordID());
+      edm::ValidityInterval interval2(runID2, runID3);
+      interval1.overlapWith(interval2);
+      REQUIRE(interval1.begin() == runID2);
+      REQUIRE(interval1.end() == interval1.end());
+    }
+
+    SECTION("overlapWith with both open-ended intervals") {
+      auto runID1 = edm::TransitionRecordID(1U);
+      auto runID2 = edm::TransitionRecordID(2U);
+      edm::ValidityInterval interval1(runID1, edm::TransitionRecordID());
+      edm::ValidityInterval interval2(runID2, edm::TransitionRecordID());
+      interval1.overlapWith(interval2);
+      REQUIRE(interval1.begin() == runID2);
+      REQUIRE(interval1.end().size() == 0);
+    }
+
+    SECTION("overlapWith with lumi-level IDs") {
+      auto runID = edm::TransitionRecordID(1U);
+      auto lumiID1 = edm::TransitionRecordID(runID, 1U);
+      auto lumiID2 = edm::TransitionRecordID(runID, 2U);
+      auto lumiID3 = edm::TransitionRecordID(runID, 3U);
+      auto lumiID4 = edm::TransitionRecordID(runID, 4U);
+      edm::ValidityInterval interval1(lumiID1, lumiID4);
+      edm::ValidityInterval interval2(lumiID2, lumiID3);
+      interval1.overlapWith(interval2);
+      REQUIRE(interval1.begin() == lumiID2);
+      REQUIRE(interval1.end() == lumiID3);
+    }
   }
 }
